ab4fcca3a5e60337ab874200e265a733e227f4e2
Leidy <Leidy@192.168.95.108>
2015-03-23 16:33:24 -0500

    PRUEBA1

--- practicap1/src/practicap1/Practicap1.java
+++ practicap1/src/practicap1/Practicap1.java
@@ -1,20 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package practicap1;
 
-/**
- *
- * @author USUARIO
- */
+
 public class Practicap1 {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         System.out.println("PEDROGO");
     }
