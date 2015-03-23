0801321f90005968e578f11f0502a5985ca009b6
CARDENAS <CARDENAS@192.168.93.46>
2015-03-23 17:13:06 -0500

    metodos de la clase auto

--- practicap1/src/practicap1/auto.java
+++ practicap1/src/practicap1/auto.java
@@ -9,10 +9,42 @@ package practicap1;
  *
  * @author CARDENAS
  */
-public class auto {
+public class auto extends vehiculo{
    String color;
    String marca;
    float placa;
    String diseño;
    String num_asientos;
-}
+   
+   public auto(String color, String marca, float placa, String diseño, String num_asientos){
+   this.color=color;
+   this.marca=marca;
+   this.placa=placa;
+   this.diseño=diseño;
+   this.num_asientos=num_asientos;
+   
+   }
+
+    /*public String getColor() {
+        return color;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getDiseño() {
+        return diseño;
+    }
+
+    public String getNum_asientos() {
+        return num_asientos;
+    }*/
+
+  public void procesar(){
+      this.encender();
+     
+      
+  }
+    }
+
