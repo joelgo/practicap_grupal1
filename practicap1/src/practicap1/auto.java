9aca90e19405dd59699dd97b6fc5a74da3a050e4
CARDENAS <CARDENAS@192.168.93.46>
2015-03-23 16:35:38 -0500

    creando la clase auto

---
+++ practicap1/src/practicap1/auto.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practicap1;
+
+/**
+ *
+ * @author CARDENAS
+ */
+public class auto {
+   String color;
+   String marca;
+   float placa;
+   String diseño;
+   String num_asientos;
+}
