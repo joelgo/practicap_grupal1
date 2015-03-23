7fa8541af4c4d0e5e9aed40f1b8a30fa52f097ff
Leidy <Leidy@192.168.95.108>
2015-03-23 15:41:10 -0500

    PRUEBA1

--- practicap1/src/practicap1/Practicap1.java
+++ practicap1/src/practicap1/Practicap1.java
@@ -16,7 +16,7 @@ public class Practicap1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("PEDROGO");
     }
     
 }
