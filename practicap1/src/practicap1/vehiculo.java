bed240cf8e3868e472ac31a0f902cffceb4f02e3
USUARIO <USUARIO@PEDRO_HP>
2015-03-23 17:08:41 -0500

    en la clase vehiculo se agrego un metodo, dando a conocer las acciones que hace el vehiculo

---
+++ practicap1/src/practicap1/vehiculo.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package practicap1;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class vehiculo {
+    
+    public void encender(){
+    
+    System.out.println("el vehiculo se encendio");
+    
+    
+    
+    }
+    
+}
