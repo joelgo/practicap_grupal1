52ceb4d50fb586bde97aec3cf9cf08013aec6638
CARDENAS <CARDENAS@192.168.1.38>
2015-03-23 18:34:18 -0500

    creando la clase piloto

---
+++ practicap1/src/practicap1/piloto.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practicap1;
+
+/**
+ *
+ * @author CARDENAS
+ */
+public class piloto {
+    String nombre;
+    int edad;
+    String rango;
+    
+    public void presentarse(){
+    System.out.println("Robert"+"19"+"copiloto");
+    
+    }
+    
+}
